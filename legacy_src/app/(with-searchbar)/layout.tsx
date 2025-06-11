b4f40bd40aeaff490a485ea36ac34ec1ade24a95
baejb <bjb6478@naver.com>
2025-06-11 17:09:23 +0900

    한입북스UI 구현

---
+++ legacy_src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import SearchBar from "../../components/searchbar";
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <SearchBar />
+      {children}
+    </div>
+  );
+}
